258b23a4438e99236b0a58b4141b728a036f137c
Dyusha12 <tamgdeya17@gmail.com>
2024-11-27 14:20:32 +0300

    Adding output to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import allclasses.irinabig.Cat;
 import allclasses.irinabig.Feline;
 import allclasses.irinabig.Lion;
 import allclasses.petrov.Programmer;
+import allclasses.yakimovich.YakimovichClass;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
         showClassesPetrov();
+        showClassesYakimovich();
 
     }
     public static void showClassesIrinaBig() throws Exception {
@@ -36,6 +38,21 @@ public class Main {
         programmer.writeCode();
         programmer.drinkCoffee();
         programmer.debugCode();
+    }
+
+    public static void showClassesYakimovich() {
+        YakimovichClass test = new YakimovichClass();
+        System.out.println("Результаты вычислений YakimovichClass:");
+        System.out.println("Сумма 5 + 3 = " + test.add(5, 3));
+        System.out.println("Разность 10 - 4 = " + test.subtract(10, 4));
+        System.out.println("Произведение 6 * 2 = " + test.multiply(6, 2));
+        try {
+            System.out.println("Деление 10 / 2 = " + test.divide(10, 2));
+            System.out.println("Деление 15 / 0 = " + test.divide(10, 0));
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+        System.out.println("Четные числа от 0 до 10: " + test.generateEvenNumbers(10));
     }
 
 }
