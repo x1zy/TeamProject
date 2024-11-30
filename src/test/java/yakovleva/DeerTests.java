c85fd69db94fe889e3d5e4b7dffcec9106a7fd36
aimoure <annaa5879@gmail.com>
2024-11-30 21:17:58 +0300

    добавление тестов

--- src/test/java/yakovleva/DeerTests.java
+++ src/test/java/yakovleva/DeerTests.java
@@ -8,18 +8,43 @@ import java.time.Period;
 
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.List;
 
 public class DeerTests 
 {
     @Test
-    void AgeCorrect() 
+    public void AgeDayBeforeToday3() 
     {
         Deer deer = new Deer("Ми", LocalDate.of(2020, 10, 12), Type.лань);
        
         int actual = deer.Age();
+
+        Period period = Period.between(deer.getBirthday(), LocalDate.now());
+        int expected = period.getYears();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void AgeDayAfterToday2() 
+    {
+        Deer deer = new Deer("Ми", LocalDate.of(2020, 12, 28), Type.лань);
+       
+        int actual = deer.Age();
         
-        Period expected = Period.between(deer.getBirthday(), LocalDate.now());
+        Period period = Period.between(deer.getBirthday(), LocalDate.now());
+        int expected = period.getYears();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void AgeDateAfterToday0() 
+    {
+        Deer deer = new Deer("Ми", LocalDate.of(2026, 12, 12), Type.лань);
+       
+        int actual = deer.Age();
+
+        int expected = 0;
 
         Assert.assertEquals(expected, actual);
     }
